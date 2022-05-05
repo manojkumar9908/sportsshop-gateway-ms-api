0d473abe1de1add5fdd7e9196bb5797ba1f1e23c
manojkumar9908 <manojroyal9908@gmail.com>
2022-05-05 18:12:21 +0530

    add src pom.xml

---
+++ src/main/java/com/sportsshop/SportsshopGatewayMsApiApplication.java
@@ -0,0 +1,14 @@
+package com.sportsshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsshopGatewayMsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsshopGatewayMsApiApplication.class, args);
+		System.out.println("gateway is working");
+	}
+
+}
---
+++ src/test/java/com/sportsshop/SportsshopGatewayMsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sportsshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsshopGatewayMsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
